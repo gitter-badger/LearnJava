0d1db712781429a44a9f256f94b35d6c1ca14f11
Sergey <aeol@yandex.ru>
2016-08-17 12:19:57 +0300

    First try

--- src/Game.java
+++ src/Game.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Game {
     private int[] board = new int[9]; //СОздаем доску 3х3, массив нумерует ячейки с верхней левой, 0-8.
-    public int moves = 1; // Число ходов. Инкремент после хода обоих игроков. Проверка победы с 3, ничья при 9.
+    public int moves = 1; // Число ходов. Проверка победы с 5, ничья при 9.
     public String playerOneName; //Имя первого игрока
     public String playerTwoName; //Имя второго игрока
     public int whoMoves = 1; // Номер игрока, который ходит. Если ходят крестики, 1, иначе 2.
@@ -32,7 +32,13 @@ public class Game {
         if(playerTwoName.length() == 0){playerTwoName = "Нолики";}
     }
 
+    // Игровой процесс
     public void game(){
-
+        while(moves < 10){ // Пока не получим подтверждение победы или не кончатся ходы
+            setBoard((int)Helper.getUserInput(whoMoves), whoMoves); // Спросим ход и запишем его на доску
+            if(Helper.checkWin == true){break;} // Проверим условия победы. Если true, выйдем из цикла к результатам.
+            // Поменяем текущего игрока.
+            // Прибавим число ходов.
+        }
     }
 }
