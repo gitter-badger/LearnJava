cf4fa1305298b458e1823c646e5952fb2afa79ee
Sergey <aeol@yandex.ru>
2016-08-18 23:49:15 +0300

    0.02

--- src/Game.java
+++ src/Game.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 class Game {
     private int[] board = new int[9]; //Создаем доску 3х3, массив нумерует ячейки с верхней левой, 0-8.
     private int moves = 1; // Число ходов. Проверку победы имеет смысл делать с 5, ничья при 9.
-    static String playerOneName; //Имя первого игрока
-    static String playerTwoName; //Имя второго игрока
+    private static String playerOneName; //Имя первого игрока
+    private static String playerTwoName; //Имя второго игрока
     private int whoMoves = 1; // Номер игрока, который ходит. Если ходят крестики, 1, иначе 2.
     // Эти же значения пишутся в ячейки доски. При успешной проверке на победу, этой переменной определяем победителя.
 
@@ -17,51 +17,43 @@ class Game {
        System.out.println("Имя игрока за нолики:");
        playerTwoName = scan.nextLine();
        if(playerTwoName.length() == 0){playerTwoName = "Нолики";}
-       scan.close();
     }
 
-    //Возвращает значение ячейки по ее индексу
+    //Возвращает значение ячейки по ее индексу. Ну и нахрена это нужно?
     public int getBoard(int i) {
         return board[i];
     }
 
     //Пишем в указанную ячейку значение в соответствии с тем, какой игрок ходит. Если уже занято, ругаемся.
     private boolean setBoard(int i, int whoMoves) {
-        if(i < 0 || i > 8) {
-            System.out.println("Ошибочный номер ячейки!");
-            return false;
-        }
-        if(board[i] != 0){
-            System.out.println("Ячейка занята");
-            return false;
-        }
-        board[i] = whoMoves;
         return true;
     }
 
     // Игровой процесс
-    public void play(){
+    void play(){
         String winner = "Draw!";
-        int playerMove = -1;
+        int playerMove;
         String playerName;
-        while(moves < 10){ // Пока не получим подтверждение победы или не кончатся ходы
+        while(moves < 9){ // Пока не получим подтверждение победы или не кончатся ходы
             // Юзер вводит номер ячейки, куда ходит
             playerName = whoMoves == 1 ? playerOneName : playerTwoName;
-            System.out.println("Ваш ход, " + playerName);
+            System.out.print("Ход " + moves + ". Ваш ход, " + playerName + ": ");
             Scanner scan = new Scanner(System.in);
-            if(scan.hasNextInt()){
-                playerMove = scan.nextInt();
-            }
+            playerMove = scan.nextInt() - 1;
             // Тут надо сделать как-то проверку, что пришло число и обработать возможный облом
-            scan.close();
-            if(setBoard(playerMove, whoMoves)){ // Запишем ход на доску
-                setBoard(playerMove, whoMoves);
-            } else {
-                System.out.println("Что-то пошло не так"); // А что, если не смогли?
+
+            // Запишем ход на доску
+            if(playerMove < 0 || playerMove > 8) {
+                System.out.println("Ошибочный номер ячейки!");
             }
-            if(Helper.checkWin(board)){ // Проверим условия победы. Если true, выйдем из цикла к результатам.
+            if(board[playerMove] != 0){
+                System.out.println("Ячейка занята");
+            }
+            board[playerMove] = whoMoves;
+
+            if(Helper.checkWin(board) && moves > 4){ // Проверим условия победы. Если true, выйдем из цикла к результатам.
                 winner = "We have a winner! And it is ";
-                winner += whoMoves == 2 ? playerOneName : playerTwoName;
+                winner += whoMoves == 1 ? playerOneName : playerTwoName;
                 break;
             }
             whoMoves = whoMoves == 2 ? 1 : 2; // Поменяем текущего игрока.
--- src/Helper.java
+++ src/Helper.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
-public final class Helper {
-
+final class Helper {
     // Проверяем, достигнуто ли одно из условий победы
-    public static boolean checkWin(int[] board){
+    static boolean checkWin(int[] board){
         boolean weHaveAWinner = false;
-        if(board[0] == board[1] && board[0]== board[2]){
+        if((board[0] == board[1] && board[0]== board[2]) || (board[3] == board[4] && board[3]== board[5]) || (board[6] == board[7] && board[6]== board[8]) || (board[0] == board[3] && board[0]== board[6]) || (board[1] == board[4] && board[1]== board[7]) || (board[2] == board[5] && board[2]== board[8]) || (board[0] == board[4] && board[0]== board[8]) || (board[2] == board[4] && board[2]== board[6])){
             weHaveAWinner = true;
         }
         return weHaveAWinner;
